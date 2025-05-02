3391d494512f874707c3a89d47afa56d9fd5c602
Nilesh <nileshkr1706@gmail.com>
2025-05-02 07:31:25 +0530

    only security left

--- src/main/java/com/app/grievance/dto/GrievanceRequest.java
+++ src/main/java/com/app/grievance/dto/GrievanceRequest.java
@@ -1,4 +1,15 @@
 package com.app.grievance.dto;
 
 public class GrievanceRequest {
+
+
+  private String comment;
+
+  public String getComment(){
+    return comment;
+  }
+
+  public void setComment(String comment){
+    this.comment = comment;
+  }
 }
